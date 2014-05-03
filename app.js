90a07d2e3a3f7357321562d8fc91ab8a1245b30c
c2sattara1@gmail.com <abdullahsattar@Abdullahs-MacBook-Air.local>
2014-05-03 08:11:22 -0400

    update:added google analytics code

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.configure(function(){
     app.use(logfmt.requestLogger());
     app.set('views', path.join(__dirname, '/client/views'));
     app.set('view engine', 'ejs');
-    app.use(express.favicon(__dirname + '/client/images/favicon.ico'));
+    //app.use(express.favicon(__dirname + '/client/images/favicon.ico'));
     app.use(express.logger('dev'));
     app.use(express.json());
     app.use(express.urlencoded());
