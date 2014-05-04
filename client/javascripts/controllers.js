90dc9b9c123339de4bedee96595243288278411a
c2sattara1@gmail.com <abdullahsattar@Abdullahs-MacBook-Air.local>
2014-05-04 11:45:56 -0400

    updated

--- client/javascripts/controllers.js
+++ client/javascripts/controllers.js
@@ -7,15 +7,41 @@ angular.module('app.controllers', ['ngCookies','ngRoute','app.factory', 'app.dir
 .controller('SignupCtrl', function($scope){
     $scope.dropdown = false;
     $scope.misMatch = false;
+    $scope.firstNameRequired = false;
+    $scope.emailRequired = false;
+    $scope.passwordRequired = false;
+    $scope.minLength = false;
     $scope.user = {};
-    $scope.signup = function(isValid) {
-        if (isValid){
-            if ($scope.user.password.localeCompare($scope.user.confirmPassword) != 0){
-                $scope.misMatch = true;
-            } 
-            else{
-                $scope.misMatch = false;
-            }
+    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    $scope.signup = function() {
+        /* re-initalize */
+        $scope.misMatch = false;
+        $scope.firstNameRequired = false;
+        $scope.emailRequired = false;
+        $scope.passwordRequired = false;
+        $scope.minLength = false;
+        $scope.invalidEmail = false;
+        
+        if ($scope.user.firstName == null || $scope.user.firstName == undefined  || $scope.user.firstName == ""){
+            $scope.firstNameRequired = true;
+        }
+        
+        if ($scope.user.email == null || $scope.user.email == undefined || $scope.user.email == ""){
+            $scope.emailRequired = true;
+        }
+        else if (!regex.test($scope.user.email)){
+            $scope.invalidEmail = true;
+        }
+        
+        if ($scope.user.password == null || $scope.user.password == undefined || $scope.user.password == ""){
+            $scope.passwordRequired = true;
+        }
+        else if ($scope.user.password.length < 6){
+            $scope.minLength = true;
+        }
+        
+        if ($scope.misMatch || $scope.firstNameRequired || $scope.emailRequired || $scope.passwordRequired || $scope.minLength || $scope.invalidEmail){
+            return;
         }
             /*$http.post('/api/register', {
                 firstName: $scope.user.firstName,
