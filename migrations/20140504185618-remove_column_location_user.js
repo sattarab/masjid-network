d0816d4d390c9b9eac6d710d8992bdbf810ffc41
c2sattara1@gmail.com <abdullahsattar@Abdullahs-MacBook-Air.local>
2014-05-04 18:57:50 -0400

    added migration support

---
+++ migrations/20140504185618-remove_column_location_user.js
@@ -0,0 +1,11 @@
+module.exports = {
+  up: function(migration, DataTypes, done) {
+    // add altering commands here, calling 'done' when finished
+    migration.removeColumn('Users', 'Location')
+    done()
+  },
+  down: function(migration, DataTypes, done) {
+    // add reverting commands here, calling 'done' when finished
+    done()
+  }
+}
